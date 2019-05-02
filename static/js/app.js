80c8c14f1950d6b5d06142be58b91f95aa06d67d
Rachel Frnka <rcfrnka@gmail.com>
2019-05-01 21:23:42 -0500

    commit

---
+++ static/js/app.js
@@ -0,0 +1,21 @@
+// from data.js
+var tableData = data;
+
+// create table
+var button = d3.select("#filter-btn");
+var inputField1 = d3.select("#datetime");
+var inputField2 = d3.select("#city");
+var tbody = d3.select("tbody");
+var resetbtn = d3.select("#reset-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+var populate = (dataInput) => {
+
+  dataInput.forEach(ufo_sightings => {
+    var row = tbody.append("tr");
+    columns.forEach(column => row.append("td").text(ufo_sightings[column])
+    )
+  });
+}
+
+populate(data);
